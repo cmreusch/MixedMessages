9fb78c1eda6d6ebaafaf8cbe6fc281caaa43834b
creusch <cameronreuschel@gmail.com>
2021-04-27 12:18:22 -0400

    Updated main js and added html

--- mixed.js
+++ mixed.js
@@ -1,2 +1,15 @@
 //Mixed messages main file
 //Created: 4-27-2021
+
+//Arrays for storing words for randomized output
+const sentenceStart = [];
+const sentenceMiddle = [];
+const sentenceEnd = [];
+
+//Function for generating the random message
+const messageGenerator = () => {
+    
+}
+
+//Tester for messageGenerator() function
+/* console.log(messageGenerator()); */
