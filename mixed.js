227fa6dc828e279504054e8a8e037a340d6af2d5
creusch <cameronreuschel@gmail.com>
2021-04-27 13:04:24 -0400

    Modified HTML file

--- mixed.js
+++ mixed.js
@@ -7,9 +7,27 @@ const sentenceMiddle = [];
 const sentenceEnd = [];
 
 //Function for generating the random message
-const messageGenerator = () => {
+function messageGenerator(){
+    /* variable to store message */
+    let generatedSentence = "";
+
+    /* implements and concatenates the message
     
+
+    /* returns the message */
+    return generatedSentence;
 }
+
+//Function for displaying message in browser
+//function displayMessage(){
+    /* generate random message */
+    //const message = messageGenerator();
+
+    /* get id for text area */
+    //const textBox = document.getElementById("output");
+
+    /* modify displayed text */
+//}
 
 //Tester for messageGenerator() function
 /* console.log(messageGenerator()); */
