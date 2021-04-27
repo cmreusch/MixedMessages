350057eaa95ba9a8157f78a0c5869461502b748f
creusch <cameronreuschel@gmail.com>
2021-04-27 13:38:33 -0400

    Finished js functionality

--- mixed.js
+++ mixed.js
@@ -2,32 +2,41 @@
 //Created: 4-27-2021
 
 //Arrays for storing words for randomized output
-const sentenceStart = [];
-const sentenceMiddle = [];
-const sentenceEnd = [];
+const start = ["The", "A"]
+const noun1 = ["dog", "tree", "person", "bird", "car", "plane"];
+const adverb = ["quietly", "lazily", "stoicly", "calmly"];
+const verb = ["walks", "drinks", "rests", "hops", "lies", "runs", "flies"];
+const preposition = ["at", "around", "besides", "away from", "far from", "in", "across"];
+const end = ["the river", "the woods", "the yard", "the beach", "the park"];
 
 //Function for generating the random message
 function messageGenerator(){
-    /* variable to store message */
-    let generatedSentence = "";
+    /* implements and concatenates the message */
+    const randomStart = start[Math.floor(Math.random() * start.length)];
+    const randomNoun1 = noun1[Math.floor(Math.random() * noun1.length)];
+    const randomAdverb = adverb[Math.floor(Math.random() * adverb.length)];
+    const randomVerb = verb[Math.floor(Math.random() * verb.length)];
+    const randomPreposition = preposition[Math.floor(Math.random() * preposition.length)];
+    const randomEnd = end[Math.floor(Math.random() * end.length)];
 
-    /* implements and concatenates the message
-    
+    const generatedSentence = randomStart + " " + randomNoun1 + " " + randomAdverb + " " + randomVerb + " " + 
+        randomPreposition + " " + randomEnd + ".";
 
     /* returns the message */
     return generatedSentence;
 }
 
 //Function for displaying message in browser
-//function displayMessage(){
+function displayMessage(){
     /* generate random message */
-    //const message = messageGenerator();
+    const message = messageGenerator();
 
     /* get id for text area */
-    //const textBox = document.getElementById("output");
+    const textBox = document.getElementById("output");
 
     /* modify displayed text */
-//}
+    textBox.innerHTML = message;
+}
 
 //Tester for messageGenerator() function
-/* console.log(messageGenerator()); */
+console.log(messageGenerator());
